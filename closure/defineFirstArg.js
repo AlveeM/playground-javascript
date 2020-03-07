954460e7bc9eedc2c1f360d7378f7a51fe6a85a1
Alvee Akand <AlveeM@users.noreply.github.com>
2020-03-06 21:33:37 -0500

    added defineFirstArg.js in closure

---
+++ closure/defineFirstArg.js
@@ -0,0 +1,9 @@
+function defineFirstArg(callback, arg) {
+    return function(...args) {
+        return callback(arg, ...args)
+    }
+}
+
+const subtract = function(big, small) { return big - small; };
+const subFrom20 = defineFirstArg(subtract, 20);
+console.log(subFrom20(5)); // should log: 15
