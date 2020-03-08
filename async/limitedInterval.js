38bc4457ebd45d61eb75d4024fca3c3cbe9b7819
Alvee Akand <AlveeM@users.noreply.github.com>
2020-03-08 00:30:25 -0500

    added limitedInterval.js in async

---
+++ async/limitedInterval.js
@@ -0,0 +1,6 @@
+function limitedInterval(callback, wait, limit) {
+    let timer = setInterval(() => callback(), wait);
+    setTimeout(() => clearInterval(timer),  limit);
+}
+
+limitedInterval(() => console.log(`repeating: ${Date.now()}`), 100, 550); // should log 'repeating' once per 100 ms, five times.
