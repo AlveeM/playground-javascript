e0564ef1547fc8046e32f6edc66c56b4817fd099
Alvee Akand <AlveeM@users.noreply.github.com>
2020-03-07 22:57:41 -0500

    added ajaxSimulate.js in async

---
+++ async/ajaxSimulate.js
@@ -0,0 +1,13 @@
+let dataReceived;
+
+function ajaxSimulate(id, callback) {
+    const database = ['Aaron', 'Barbara', 'Chris'];
+    setTimeout(() => callback(database[id]), 0);
+}
+
+function storeData(data) {
+    dataReceived = data;
+    console.log(dataReceived);
+}
+
+ajaxSimulate(1, storeData);
