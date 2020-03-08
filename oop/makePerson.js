e39c81a95b5ed7a043e4de0b4ecedcd950717105
Alvee Akand <AlveeM@users.noreply.github.com>
2020-03-08 18:43:10 -0400

    added makePerson.js in oop

---
+++ oop/makePerson.js
@@ -0,0 +1,10 @@
+function makePerson(name, age) {
+    let obj = {};
+    obj.name = name;
+    obj.age = age;
+    return obj
+}
+
+const vicky = makePerson('Vicky', 24);
+console.log(vicky.name); // -> Logs 'Vicky'
+console.log(vicky.age); // -> Logs 24
