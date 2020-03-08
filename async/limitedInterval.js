88c8e45e7598834f7ac792f9fdcb870e56daca24
Alvee Akand <AlveeM@users.noreply.github.com>
2020-03-08 18:19:46 -0400

    refactored limitedInterval.js to remove anonymous functions from setInterval and setTimeout

--- async/limitedInterval.js
+++ async/limitedInterval.js
@@ -1,6 +1,8 @@
 function limitedInterval(callback, wait, limit) {
-    let timer = setInterval(() => callback(), wait);
-    setTimeout(() => clearInterval(timer),  limit);
+    // let timer = setInterval(() => callback(), wait);
+    // setTimeout(() => clearInterval(timer),  limit);
+    let timer = setInterval(callback, wait);
+    setTimeout(clearInterval,  limit, timer);
 }
 
 limitedInterval(() => console.log(`repeating: ${Date.now()}`), 100, 550); // should log 'repeating' once per 100 ms, five times.
