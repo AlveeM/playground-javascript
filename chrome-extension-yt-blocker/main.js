6f6635ab2c97dc0e8c837eb173a3fafaf0b2db69
Alvee Akand <AlveeM@users.noreply.github.com>
2020-03-08 23:15:00 -0400

    added css

--- chrome-extension-yt-blocker/main.js
+++ chrome-extension-yt-blocker/main.js
@@ -8,5 +8,6 @@ const img = `
     <img src='https://www.unilad.co.uk/wp-content/uploads/2018/07/success-kid-original.jpg' alt='success kid'>
 `;
 const message = `<p>Focus on your tasks and achieve your dreams!</p>`
+contentsParent.addClass('beautText');
 contentsParent.prepend(img);
 contentsParent.prepend(message);
