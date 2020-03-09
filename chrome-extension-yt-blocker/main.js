10ab7a730f14c915978353b5748eba374f952f5d
Alvee Akand <AlveeM@users.noreply.github.com>
2020-03-08 22:49:59 -0400

    added jquery and refactored code

--- chrome-extension-yt-blocker/main.js
+++ chrome-extension-yt-blocker/main.js
@@ -1,2 +1,12 @@
-const contents = document.getElementById('page-manager');
-contents.parentNode.removeChild(contents);
+// const contents = document.getElementById('content');
+// contents.parentNode.removeChild(contents);
+
+const contents = $('#content');
+const contentsParent = contents.parent();
+contents.remove();
+const img = `
+    <img src='https://www.unilad.co.uk/wp-content/uploads/2018/07/success-kid-original.jpg' alt='success kid'>
+`;
+const message = `<p>Focus on your tasks and achieve your dreams!</p>`
+contentsParent.prepend(img);
+contentsParent.prepend(message);
