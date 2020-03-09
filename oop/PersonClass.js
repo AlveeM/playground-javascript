526615a691273aa7afd6873d67905ec9dc6dfb0f
Alvee Akand <AlveeM@users.noreply.github.com>
2020-03-08 20:53:33 -0400

    added PersonClass.js in oop

---
+++ oop/PersonClass.js
@@ -0,0 +1,14 @@
+class PersonClass {
+    constructor(name) {
+        this.name = name;
+    }
+
+    greet() {
+        console.log('hello');
+    }
+
+}
+
+const george = new PersonClass('George');
+
+george.greet(); // -> Logs 'hello'
