6531520bc0fd3e307a77da50e00f6986141c143d
Alvee Akand <AlveeM@users.noreply.github.com>
2020-03-11 16:58:50 -0400

    added curry-compose in callback-hof

---
+++ callback-hof/curry-compose.js
@@ -0,0 +1,14 @@
+const curry = (fn) => {
+    return (arg) => {
+        return (arg2) => {
+            return fn(arg, arg2);
+        }
+    }
+}
+
+const compose = (fn, fn2) => {
+    return (arg) => {
+        const result = fn2(arg);
+        return fn(result);
+    }
+}
